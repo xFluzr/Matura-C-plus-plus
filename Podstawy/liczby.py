2243834749ae8d1a11082891e242173645bd81a6
Jakub <jakub.jaszcz538@gmail.com>
2022-09-05 22:05:50 +0200

    Added python numbers and strings

---
+++ Podstawy/liczby.py
@@ -0,0 +1,38 @@
+# dużo fajnych rzeczy stąd jest
+from math import *
+
+# IV Liczby całkowite i rzeczywiste
+
+# Całkowite
+# print(4 + 3)
+# print(5 % 4)
+
+# Rzeczywiste
+# print(4.5 * 4)
+# print(4.3 - 5.3)
+
+# przekształcanie liczby w stringa
+# my_number = 5
+# print(str(my_number))
+
+# Wartość bezwględna
+# print(abs(-10))
+
+# Potęgowanie
+# print(pow(5, 2))
+
+#Zaokrąglanie (druga wartość funkcji to liczby po przecinku)
+# print(round(5.3123121,2))
+
+#Zaokrąglanie w dół
+# print(floor(4.3))
+
+#Zaokrąglanie w górę
+# print(ceil(4.5))
+
+#Pierwiastek
+# print(sqrt(16))
+
+#Liczba PI
+# print(pi)
+
---
+++ Podstawy/zmienne_i_stringi.py
@@ -0,0 +1,53 @@
+"""
+I. Print - wypisuje w konsoli to jest podane jako argmunet
+"""
+
+# print("Hello World")
+# age = 19
+#Przykład formatowania tekstu ze zmienną
+# print(f"Hi my name is {age}")
+#
+
+
+#II . Zmienne (variables)
+
+# age = 19
+# studentStatus= False
+# name = "Jakub"
+# sentence= "I don't like c++"
+#
+# print(f"Hi my name is {name}, I'm {age}, {sentence}")
+
+
+#III. Funkcje do stringów
+
+# phrase="It is to pass matura with python"
+# sentence="dog"
+#wszystkie litery duże
+# print(sentence.upper())
+#
+# wszystkie litery małe
+# print(sentence.lower())
+#
+# Można używać kilka na raz
+# print(sentence.upper().isupper())
+#
+# długość stringa
+# print(len(phrase))
+#
+# string działa jak tablica
+# print(phrase[0])
+#
+# ostatnia litera jak wybrać
+# print(phrase[-1])
+#
+# odwracanie stringa
+# print(phrase[::-1])
+#
+# index jakieś litery
+# print(phrase.index("t"))
+#
+#zamiana jakiś litery albo wyrazu
+# print(phrase.replace("pass", "lol"))
+
+
